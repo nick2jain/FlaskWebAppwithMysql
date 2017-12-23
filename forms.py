57d0c0edc33270860b7b886093d67fa416f44b18
nick2jain <jain2nikhil@gmail.com>
2017-12-23 01:27:58 -0600

    Delete forms.py

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Email, DataRequired
-class SignupFormdemo(Form):
-    email = StringField('email',validators=[DataRequired(),Email()])
-    password = PasswordField('password',validators=[DataRequired()])
-    submit = SubmitField("Sign In")
